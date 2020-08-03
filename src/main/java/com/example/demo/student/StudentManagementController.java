285c39e7a14b626f820b34d20f16379fb9d46151
Jacek135 <jac.poczatek@gmail.com>
2020-08-03 20:18:28 +0200

    From home

---
+++ src/main/java/com/example/demo/student/StudentManagementController.java
@@ -0,0 +1,31 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("management/api/v1/students")
+public class StudentManagementController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "James Bond"),
+            new Student(2, "Maria Jones"),
+            new Student(3, "Anna Smith")
+    );
+
+    public List<Student> getAllStudents() {
+        return STUDENTS;
+    }
+
+    public void registerNewStudent(Student student) {
+        System.out.println(student);
+    }
+
+    public void deleteStudent(Integer studentId) {
+        
+    }
+
+}
